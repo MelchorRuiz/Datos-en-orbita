e3d902e247734b5e0dcdc9de99cb8b393f84f342
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-12-03 14:32:22 +0100

    Refactor image processing to accept file

--- data/cut_images.py
+++ data/cut_images.py
@@ -14,25 +14,31 @@ def crop_image(filename, folder_path, output_folder, crop_coords):
   try:
     image_path = os.path.join(folder_path, filename)
     image = cv2.imread(image_path)
+
     if image is None:
       raise ValueError(f"Cannot load image: {image_path}")
+    
     x, y, width, height = crop_coords
     cropped_image = image[y:y + height, x:x + width]
+
     if cropped_image is not None:
       output_path = os.path.join(output_folder, filename)
       cv2.imwrite(output_path, cropped_image)
       print(f"Processed and saved: {output_path}")
     else:
       print(f"Skipping file due to error: {image_path}")
+      
   except Exception as e:
     print(f"Error cropping image {image_path}: {e}")
 
-def process_images_in_folder(folder_path, delete_originals=False):
+def process_images_in_folder(files, folder_path,delete_originals=False):
   """
   Process all image files in a folder by cropping them based on the given coordinates.
 
   Args:
-    folder_path (str): Path to the folder containing image files.
+    files (list): List of image files in the folder.
+    folder_path (str): Path to the folder containing the image files.
+    delete_originals (bool): Whether to delete the original images after processing.
   """
   
   def get_output_folder(folder):
@@ -46,20 +52,16 @@ def process_images_in_folder(folder_path, delete_originals=False):
     output_folder_altitude_superheavy = get_output_folder('altitude_superheavy')
     output_folder_speed_starship = get_output_folder('speed_starship')
     output_folder_altitude_starship = get_output_folder('altitude_starship')
-    for root, _, files in os.walk(folder_path):
-      if root == folder_path:
-        for filename in files:
-          crop_image(filename, folder_path, output_folder_time, (1283, 525, 94, 16))
-          crop_image(filename, folder_path, output_folder_spped_superheavy, (1224, 507, 64, 12))
-          crop_image(filename, folder_path, output_folder_altitude_superheavy, (1043, 524, 46, 12))
-          crop_image(filename, folder_path, output_folder_speed_starship, (1578, 508, 64, 12))
-          crop_image(filename, folder_path, output_folder_altitude_starship, (1596, 524, 46, 12))
-          if delete_originals:
-            os.remove(os.path.join(folder_path, filename))
-        break
+
+    for filename in files:
+      crop_image(filename, folder_path, output_folder_time, (1283, 525, 94, 16))
+      crop_image(filename, folder_path, output_folder_spped_superheavy, (1224, 507, 64, 12))
+      crop_image(filename, folder_path, output_folder_altitude_superheavy, (1043, 524, 46, 12))
+      crop_image(filename, folder_path, output_folder_speed_starship, (1578, 508, 64, 12))
+      crop_image(filename, folder_path, output_folder_altitude_starship, (1596, 524, 46, 12))
+
+      if delete_originals:
+        os.remove(os.path.join(folder_path, filename))
+
   except Exception as e:
     print(f"Error processing folder {folder_path}: {e}")
-
-if __name__ == '__main__':
-  process_images_in_folder('screenshots/flight_6')
-  
--- data/main.py
+++ data/main.py
@@ -1,16 +1,24 @@
+import os
 from capture_screenshots import from_x
 from cut_images import process_images_in_folder
 
 broadcasts = [
   {
+    'id': 'flight_6',
     'url' : 'https://x.com/i/broadcasts/1RDGlydZAeOJL',
-    'output_folder' : 'screenshots/flight_6',
     'duration': 109,
     'platform': 'x'
   }
 ]
 
+def get_files(folder_path):
+  for root, _, files in os.walk(folder_path):
+    if root == folder_path:
+      return files
+
 for broadcast in broadcasts:
   if (broadcast['platform'] == 'x'):
     from_x(broadcast['url'], broadcast['output_folder'], broadcast['duration'])
-  process_images_in_folder(broadcast['output_folder'])
+  folder_path = 'screenshots/' + broadcast['id']
+  files = get_files(folder_path)
+  process_images_in_folder(files, folder_path, delete_originals=True)
