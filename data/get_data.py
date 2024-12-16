eb275cc3f5858c4cb88593cb8a9c40229a157d19
MelchorRuiz <ruizgonzalezmelchor3@gmail.com>
2024-12-16 16:30:25 +0100

    Use a firefox browser

--- data/get_data.py
+++ data/get_data.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from time import sleep
 import os
 from datetime import datetime
@@ -15,13 +12,13 @@ def from_x(live_url, output_folder, duration=10):
       output_folder (str): Folder to save the screenshots.
       duration (int): Duration in minutes to capture screenshots.
   """
-  chrome_options = Options()
-  chrome_options.add_argument("--headless")
-  chrome_options.add_argument("--window-size=1920,1080")
+  options = webdriver.FirefoxOptions()
+  options.add_argument("--headless")
+  options.add_argument("--window-size=1920,1080")
 
   os.makedirs(output_folder, exist_ok=True)
   
-  driver = webdriver.Chrome(options=chrome_options)
+  driver = webdriver.Firefox()
 
   try:
     driver.get(live_url)
--- data/main.py
+++ data/main.py
@@ -1,7 +1,7 @@
 import os
-from capture_screenshots import from_x
+from get_data import from_x
 from cut_images import crop_images_in_folder
-from process_images import process_images
+# from process_images import process_images
 
 broadcasts = [
   {
@@ -18,10 +18,9 @@ def get_files(folder_path):
       return files
 
 for broadcast in broadcasts:
-  if (broadcast['platform'] == 'x'):
-    pass
-    # from_x(broadcast['url'], broadcast['output_folder'], broadcast['duration'])
   folder_path = 'screenshots/' + broadcast['id']
+  if (broadcast['platform'] == 'x'):
+    from_x(broadcast['url'], folder_path, broadcast['duration'])
   files = get_files(folder_path)
   # crop_images_in_folder(files, folder_path, delete_originals=False)
-  process_images(broadcast['id'], folder_path, files, delete_originals=False)
+  # process_images(broadcast['id'], folder_path, files, delete_originals=False)
